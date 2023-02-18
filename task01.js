cbdb7ffc33ada5e054a6ce1b2d71ee558ae25408
truptimahajan1987 <123890990+truptimahajan1987@users.noreply.github.com>
2023-02-18 07:39:46 +0530

    First Commit

---
+++ task01.js
@@ -0,0 +1,16 @@
+function bulbon(){
+    document.getElementById("bulboff").src="./img/bulb-on.png"
+
+}
+function bulboff(){
+    document.getElementById("bulboff").src="./img/bulb-off.png"
+
+}
+function hidebulb(){
+    document.getElementById("bulboff").style.display="none"
+
+}
+function showbulb(){
+
+    document.getElementById("bulboff").style.display="block"
+}
